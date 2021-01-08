13343b0aa7de000faa2c76f51ae89e7645ffdad6
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-01-08 17:25:37 +0000

    corrected lacking components and syntax
    created const client
    created const db
    corrected syntax on res.status

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -29,10 +29,11 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
   try {
     const articleName = req.params.name;
 
-    const articleInfo = await db.collection('articles').findOne({ name: articleName });
-    const db = client.db('my-blog')
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('my-blog');
 
-    
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+
     await db.collection('articles').updateOne({ name: articleName }, {
       '$set': {
         upvotes: articleInfo.upvotes + 1,
@@ -40,7 +41,7 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
     });
     const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
 
-    res.status(100).json(updatedArticleInfo);
+    res.status(200).json(updatedArticleInfo);
 
     client.close();
   } catch (error) {
