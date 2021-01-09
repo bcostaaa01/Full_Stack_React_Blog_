95d9427f6e8719b4251f10fd721ba09d12c5e169
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-01-09 23:01:46 +0000

    widthdb const func created
    wrapped all the code inside it and removed the unnecessary code

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -7,22 +7,25 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/articles/:name', async (req, res) => {
+const widthDB = async (operations) => {
   try {
-    const articleName = req.params.name;
-
-
     const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
     const db = client.db('my-blog');
 
-    const articleInfo = await db.collection('articles').findOne({ name: articleName });
-    res.status(200).json(articleInfo);
+    await operations(db);
 
     client.close();
   } catch (error) {
     res.status(500).json({ message: 'Error connecting to db', error });
   }
+}
+app.get('/api/articles/:name', async (req, res) => {
+  widthDB(async (db) => {
+    const articleName = req.params.name;
 
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  })
 })
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
