00f114ffa36cab47474381ecad3ad2fdaa2a778f
Bruno <bcostinha01@gmail.com>
2021-01-12 16:36:45 +0000

    react hook created

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArticlesList from '../Components/ArticlesList';
 import NotFoundPage from '../pages/NotFoundPage';
 import articleContent from './article-content';
 
 
 const ArticlePage = ({ match }) => {
-
     const name = match.params.name;
-
     const article = articleContent.find(article => article.name)
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] }); 
+
     if (!article) return <NotFoundPage />
-    
+
     const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
-    
+
     <>
         <h1>{article.title}</h1>
-        
+
         {
         article.content.map((paragraph, key) => (
         <p key={key}>
