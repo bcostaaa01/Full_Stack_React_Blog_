fca89db54559949d5ee9c0b4c048d13164229e65
Bruno <bcostinha01@gmail.com>
2021-02-01 15:28:43 +0000

    Update index.js
    Update ReactDOM.render()

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -5,10 +5,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render (
+  <App />,
   document.getElementById('root')
 );
 
